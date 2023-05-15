9be532acb1997febc8776d1060b9b83a3857f7c2
Yuri Ilyushin <yurbass1@rambler.ru>
2023-05-15 14:54:40 +0300

    Cleaned up a bit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,5 +8,11 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
