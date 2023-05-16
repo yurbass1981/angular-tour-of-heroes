a29c2a5e4971d16a6f7d5bceb556ff86c84d3b6c
Yuri Ilyushin <yurbass1@rambler.ru>
2023-05-16 12:57:31 +0300

    Added method

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,14 +10,23 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent {
 
-  heroes: Hero[] = [];
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) {
     
   }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
