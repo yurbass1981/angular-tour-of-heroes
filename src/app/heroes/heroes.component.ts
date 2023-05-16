3c2fbc661ba8b2c925ded855cc398e2a75b0673b
Yuri Ilyushin <yurbass1@rambler.ru>
2023-05-16 12:28:53 +0300

    Added constructor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
+
 export class HeroesComponent {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {
+    
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
