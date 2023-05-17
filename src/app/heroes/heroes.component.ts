9b4e15ad0b66594091c68a49ebf7a6ca3e6d9ce2
Yuri Ilyushin <yurbass1@rambler.ru>
2023-05-17 13:52:25 +0300

    Finished navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,26 +10,17 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 
-export class HeroesComponent {
-
-  selectedHero?: Hero;
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {
-    
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
