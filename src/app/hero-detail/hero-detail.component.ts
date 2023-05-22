2234b06cbd30b6b4b276b4c8409071db198b6fc7
Yuri Ilyushin <yurbass1@rambler.ru>
2023-05-22 16:40:00 +0300

    Added new components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,4 +33,11 @@ export class HeroDetailComponent {
     goBack(): void {
       this.location.back();
     }
+
+    save(): void {
+      if (this.hero) {
+        this.heroService.updateHero(this.hero)
+          .subscribe(() => this.goBack());
+      }
+    }
 }
